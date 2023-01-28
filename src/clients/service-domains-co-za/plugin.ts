c194fc0331d2790ce71b1668c17e48c461d97c10
Mitchell R <mitchell.robert@bettercorp.co.za>
2023-01-28 13:47:01 +0200

    Added client and default export

---
+++ src/clients/service-domains-co-za/plugin.ts
@@ -0,0 +1,72 @@
+import {
+  ServicesClient,
+  ServiceCallable,
+  ServicesBase,
+} from "@bettercorp/service-base";
+import { DomainsCoZaReturnableEvents } from "../../plugins/service-domains-co-za/plugin";
+import { DomainsPluginConfig } from "../../plugins/service-domains-co-za/sec.config";
+import { DomainCheckResponse } from "../../plugins/service-domains-co-za/domains";
+
+export class domainsCoZa extends ServicesClient<
+  ServiceCallable,
+  ServiceCallable,
+  DomainsCoZaReturnableEvents,
+  ServiceCallable,
+  ServiceCallable,
+  DomainsPluginConfig
+> {
+  public readonly _pluginName: string = "service-domains-co-za";
+  constructor(self: ServicesBase) {
+    super(self);
+  }
+
+  public async checkDomain(
+    sid: string,
+    tld: string
+  ): Promise<DomainCheckResponse>;
+  public async checkDomain(
+    sid: string,
+    tld: string,
+    username: string,
+    key: string
+  ): Promise<DomainCheckResponse>;
+  public async checkDomain(
+    sid: string,
+    tld: string,
+    username?: string,
+    key?: string
+  ): Promise<DomainCheckResponse> {
+    return await this._plugin.emitEventAndReturn(
+      "checkDomainSingleTLD",
+      sid,
+      tld,
+      username,
+      key
+    );
+  }
+
+  public async checkDomainTLDs(
+    sid: string,
+    tlds: Array<string>,
+  ): Promise<DomainCheckResponse>;
+  public async checkDomainTLDs(
+    sid: string,
+    tlds: Array<string>,
+    username: string,
+    key: string
+  ): Promise<DomainCheckResponse>;
+  public async checkDomainTLDs(
+    sid: string,
+    tlds: Array<string>,
+    username?: string,
+    key?: string
+  ): Promise<DomainCheckResponse> {
+    return await this._plugin.emitEventAndReturn(
+      "checkDomainMultipleTLDs",
+      sid,
+      tlds,
+      username,
+      key
+    );
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { domainsCoZa } from "./clients/service-domains-co-za/plugin";
+export {
+  DomainsDefaultResponse,
+  DomainCheckResponse,
+  DomainCheckTLDSResponse,
+  DomainCheckTLDSResponseTLD,
+  ObjReseller,
+  DomainsGetPricesResponsePrice,
+  Currency,
+} from "./plugins/service-domains-co-za/domains";
